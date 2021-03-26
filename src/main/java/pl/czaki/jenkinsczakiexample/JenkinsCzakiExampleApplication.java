f5fb7ad3126ca3980569e7b897665f35246673e0
Cezary.Polosak <cezary.polosak@plus.pl>
2021-03-26 16:54:51 +0100

    first commit

---
+++ src/main/java/pl/czaki/jenkinsczakiexample/JenkinsCzakiExampleApplication.java
@@ -0,0 +1,13 @@
+package pl.czaki.jenkinsczakiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCzakiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsCzakiExampleApplication.class, args);
+    }
+
+}
